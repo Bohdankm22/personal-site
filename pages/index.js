c15467f58d960f0ae2a3a549077f68d327c64ea0
Bohdan S <bohdansharipov@gmail.com>
2020-07-09 10:05:39 -0400

    Updated title

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <Layout home>
       <Head>
-        <title>{siteTitle}</title>
+        <title>Bohdan's world</title>
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
